a7ee0e19e815f86dcf26b98ae78aa34b03fa6d4e
hindenbug <304184+hindenbug@users.noreply.github.com>
2020-12-05 21:27:41 +0100

    Add Day 5 Part 2 solution, refactoring

--- go/05/day05.go
+++ go/05/day05.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math"
 	"strings"
 )
 
-var rows = make([]int, 128)
-
 func main() {
 	data, err := ioutil.ReadFile("input.txt")
-	highestSeatID := 0
+
+	var highestSeatID, lowestSeatID int
 
 	if err != nil {
 		log.Fatal(err)
@@ -19,64 +19,55 @@ func main() {
 
 	boardingPasses := strings.Split(string(data), "\n")
 
+	var seatIDs [128 * 8]bool
+
 	for _, boardingPass := range boardingPasses {
 		seatID := findSeatID(boardingPass)
 		if seatID > highestSeatID {
 			highestSeatID = seatID
 		}
+		if seatID < lowestSeatID {
+			lowestSeatID = seatID
+		}
+
+		seatIDs[seatID] = true
+	}
+
+	var mySeat int
+	for i, taken := range seatIDs[lowestSeatID:highestSeatID] {
+		if !taken {
+			mySeat = lowestSeatID + i
+		}
 	}
 
 	fmt.Println(highestSeatID)
+	fmt.Println(mySeat)
 }
 
-func findSeatID(boardingPass string) int {
-	return getRow(boardingPass)*8 + getCol(boardingPass)
+func findSeatID(input string) int {
+	row, col := search(127, input[:7]), search(7, input[7:])
+	return (row * 8) + col
 }
 
-func getRow(code string) int {
-	minRow := 0
-	maxRow := 127
+func search(max int, code string) int {
+	lower := 0
+	upper := max
 
-	substr := code[:7]
+	for i, char := range code {
+		if string(char) == "F" || string(char) == "L" {
+			upper -= int(math.Ceil(float64(upper-lower) / 2))
+			if i == len(code)-1 {
+				return lower
+			}
+		}
 
-	for _, char := range substr {
-		maxRow, minRow = setRange(string(char), maxRow, minRow)
+		if string(char) == "B" || string(char) == "R" {
+			lower += int(math.Ceil(float64(upper-lower) / 2))
+			if i == len(code)-1 {
+				return upper
+			}
+		}
 	}
 
-	if string(substr[len(substr)-1]) == "F" {
-		return minRow
-	} else {
-		return maxRow
-	}
-}
-
-func getCol(code string) int {
-	minCol := 0
-	maxCol := 7
-
-	substr := code[7:]
-
-	for _, char := range substr {
-		maxCol, minCol = setRange(string(char), maxCol, minCol)
-	}
-
-	if string(substr[len(substr)-1]) == "L" {
-		return minCol
-	} else {
-		return maxCol
-	}
-}
-
-func setRange(char string, upper int, lower int) (int, int) {
-	newUpper, newLower := upper, lower
-
-	switch string(char) {
-	case "F", "L":
-		newUpper = (upper + lower) / 2
-	case "B", "R":
-		newLower = (upper+lower)/2 + 1
-
-	}
-
-	return newUpper, newLower
+	return 0
 }
